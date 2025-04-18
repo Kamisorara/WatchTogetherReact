3878ef62d77f4bb8571ea6b33d734bd0c10943c6
Kamisora <rakamiso575@gmail.com>
2025-04-18 17:47:42 +0800

    初始化项目

---
+++ README.md
@@ -0,0 +1,4 @@
+# WatchTogetherReact 
+## React + Typescript + Tailwindcss
+[后端](https://github.com/Kamisorara/WatchTogetherBackend)
+
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+      <p className='text-blue-200'>This is AtlasKK</p>
+    </>
+  )
+}
+
+export default App
