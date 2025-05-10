536b32e3f7eb7fd544bb23ed7e7a6cb04302a63d
Kamisora <rakamiso575@gmail.com>
2025-05-10 14:28:41 +0800

    配置开发环境，替换图标

--- vite.config.ts
+++ vite.config.ts
@@ -7,17 +7,5 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     port: 5000,
-    // 开发环境代理配置
-    proxy: {
-      '/': {
-        target: 'http://127.0.0.1:8081',
-        changeOrigin: true
-      },
-      '/websocket': {
-        target: 'http://127.0.0.1:8081',
-        changeOrigin: true,
-        ws: true
-      }
     }
-  }
 })
